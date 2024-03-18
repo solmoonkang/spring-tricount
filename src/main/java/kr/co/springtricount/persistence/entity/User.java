262c864c568ba943e77ec03b3ba087a01c79986f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-19 01:24:18 +0900

    feat: 회원 엔티티 생성

---
+++ src/main/java/kr/co/springtricount/persistence/entity/User.java
@@ -0,0 +1,38 @@
+package kr.co.springtricount.persistence.entity;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import kr.co.springtricount.persistence.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tbl_users")
+@AttributeOverride(
+        name = "id",
+        column = @Column(name = "user_id", length = 4)
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Column(name = "identity", length = 50, nullable = false, unique = true)
+    private String identity;
+
+    @Column(name = "password", length = 50, nullable = false)
+    private String password;
+
+    @Column(name = "name", length = 30, nullable = false)
+    private String name;
+
+    @Builder
+    public User(String identity,
+                String password,
+                String name) {
+        this.identity = identity;
+        this.password = password;
+        this.name = name;
+    }
+}
