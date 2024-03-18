e9b40f528d27b01d27c67c4be32b4666f00116a5
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-19 01:54:47 +0900

    feat: 회원 아이디 존재 여부를 반환하는 로직 구현

--- src/main/java/kr/co/springtricount/persistence/repository/UserRepository.java
+++ src/main/java/kr/co/springtricount/persistence/repository/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    public boolean existsUserByIdentity(String identity);
+    boolean existsUserByIdentity(String identity);
 }
