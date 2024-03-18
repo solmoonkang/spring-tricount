5c9a1a9ae384b97b5a52c95270068db5950d7c27
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-19 01:44:15 +0900

    feat: 회원 생성 DTO 구현

---
+++ src/main/java/kr/co/springtricount/service/dto/request/CreateMemberReqDTO.java
@@ -0,0 +1,9 @@
+package kr.co.springtricount.service.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateMemberReqDTO(
+        @NotBlank(message = "아이디를 입력해주세요.") String identity,
+        @NotBlank(message = "비밀번호를 입력해주세요.") String password,
+        @NotBlank(message = "이름을 입력해주세요.") String name
+) { }
