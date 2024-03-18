d1e59c7de4c7e4b3153253278b329919bbf82e10
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-19 01:49:08 +0900

    feat: 회원 생성 컨트롤러 구현

---
+++ src/main/java/kr/co/springtricount/controller/UserController.java
@@ -0,0 +1,28 @@
+package kr.co.springtricount.controller;
+
+import kr.co.springtricount.service.UserService;
+import kr.co.springtricount.service.dto.request.CreateMemberReqDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody @Validated CreateMemberReqDTO create) {
+
+        userService.createUser(create);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
