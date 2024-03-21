75372b122f817275b1d9d499b93c8df2f6ed287a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-22 00:51:53 +0900

    chore: 로그인과 로그아웃 기능 제거

--- src/main/java/kr/co/springtricount/controller/MemberController.java
+++ src/main/java/kr/co/springtricount/controller/MemberController.java
@@ -1,13 +1,11 @@
 package kr.co.springtricount.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
+import kr.co.springtricount.infra.response.ResponseFormat;
+import kr.co.springtricount.infra.response.ResponseStatus;
 import kr.co.springtricount.service.MemberService;
-import kr.co.springtricount.service.dto.request.LoginDTO;
 import kr.co.springtricount.service.dto.request.MemberReqDTO;
 import kr.co.springtricount.service.dto.response.MemberResDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,49 +19,36 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping
-    public ResponseEntity<Void> createMember(@RequestBody @Validated MemberReqDTO create) {
+    public ResponseFormat<Void> createMember(@RequestBody @Validated MemberReqDTO create) {
 
         memberService.createMember(create);
 
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<Void> login(HttpServletRequest request,
-                                      @RequestBody @Validated LoginDTO loginDTO) {
-
-        memberService.login(request, loginDTO);
-
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
-
-    @PostMapping("/logout")
-    public ResponseEntity<Void> logout(HttpServletRequest request) {
-
-        memberService.logout(request);
-
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseFormat.successMessage(ResponseStatus.SUCCESS_CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<List<MemberResDTO>> findAllMember() {
+    public ResponseFormat<List<MemberResDTO>> findAllMember() {
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(memberService.findAllMember());
+        return ResponseFormat.successMessageWithData(
+                ResponseStatus.SUCCESS_EXECUTE,
+                memberService.findAllMember()
+        );
     }
 
     @GetMapping("/{identity}")
-    public ResponseEntity<MemberResDTO> findMemberByIdentity(@PathVariable String identity) {
+    public ResponseFormat<MemberResDTO> findMemberByIdentity(@PathVariable String identity) {
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(memberService.findMemberByIdentity(identity));
+        return ResponseFormat.successMessageWithData(
+                ResponseStatus.SUCCESS_EXECUTE,
+                memberService.findMemberByIdentity(identity)
+        );
     }
 
     @DeleteMapping("/{identity}")
-    public ResponseEntity<Void> deleteMember(@PathVariable String identity) {
+    public ResponseFormat<Void> deleteMember(@PathVariable String identity) {
 
         memberService.deleteMember(identity);
 
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseFormat.successMessage(ResponseStatus.SUCCESS_EXECUTE);
     }
 }
