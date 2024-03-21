f6a997858758ed314ab52e7ecd00d96170a23df5
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-22 00:51:06 +0900

    feat: 로직 예외 메시지를 예외 상태로 변경

--- src/main/java/kr/co/springtricount/service/MemberService.java
+++ src/main/java/kr/co/springtricount/service/MemberService.java
@@ -1,12 +1,10 @@
 package kr.co.springtricount.service;
 
-import jakarta.servlet.http.HttpServletRequest;
 import kr.co.springtricount.infra.exception.DuplicatedException;
 import kr.co.springtricount.infra.exception.NotFoundException;
-import kr.co.springtricount.infra.exception.UnauthorizedException;
+import kr.co.springtricount.infra.response.ResponseStatus;
 import kr.co.springtricount.persistence.entity.Member;
 import kr.co.springtricount.persistence.repository.MemberRepository;
-import kr.co.springtricount.service.dto.request.LoginDTO;
 import kr.co.springtricount.service.dto.request.MemberReqDTO;
 import kr.co.springtricount.service.dto.response.MemberResDTO;
 import lombok.RequiredArgsConstructor;
@@ -33,21 +31,6 @@ public class MemberService {
         memberRepository.save(member);
     }
 
-    public void login(HttpServletRequest request, LoginDTO loginDTO) {
-
-        final Member findMember = memberRepository.findMemberByIdentity(loginDTO.identity())
-                .orElseThrow(() -> new NotFoundException("요청하신 회원은 없는 회원입니다."));
-
-        checkPasswordMatch(findMember.getPassword(), loginDTO.password());
-
-        request.getSession().setAttribute("member", findMember.getIdentity());
-    }
-
-    public void logout(HttpServletRequest request) {
-
-        request.getSession().invalidate();
-    }
-
     public List<MemberResDTO> findAllMember() {
 
         final List<Member> findMember = memberRepository.findAll();
@@ -60,7 +43,7 @@ public class MemberService {
     public MemberResDTO findMemberByIdentity(String identity) {
 
         final Member findMember = memberRepository.findMemberByIdentity(identity)
-                .orElseThrow(() -> new NotFoundException("요청하신 회원은 없는 회원입니다."));
+                .orElseThrow(() -> new NotFoundException(ResponseStatus.FAIL_MEMBER_NOT_FOUND));
 
         return findMember.toReadDto();
     }
@@ -69,20 +52,14 @@ public class MemberService {
     public void deleteMember(String identity) {
 
         final Member deleteMember = memberRepository.findMemberByIdentity(identity)
-                .orElseThrow(() -> new NotFoundException("요청하신 회원은 없는 회원입니다."));
+                .orElseThrow(() -> new NotFoundException(ResponseStatus.FAIL_MEMBER_NOT_FOUND));
 
         memberRepository.delete(deleteMember);
     }
 
     private void checkIdentityExists(String identity) {
         if (memberRepository.existsMemberByIdentity(identity)) {
-            throw new DuplicatedException("해당 아이디는 이미 존재하는 아이디입니다.");
-        }
-    }
-
-    private void checkPasswordMatch(String storedPassword, String inputPassword) {
-        if (!storedPassword.equals(inputPassword)) {
-            throw new UnauthorizedException("일치하지 않는 회원입니다.");
+            throw new DuplicatedException(ResponseStatus.FAIL_IDENTITY_DUPLICATION);
         }
     }
 }
