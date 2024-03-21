8c2a16cd2c87689575b22470ddc1781a8eaa7b37
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-22 01:20:12 +0900

    feat: 예외 핸들러 구현

---
+++ src/main/java/kr/co/springtricount/infra/handler/GlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package kr.co.springtricount.infra.handler;
+
+import kr.co.springtricount.infra.exception.AuthenticationException;
+import kr.co.springtricount.infra.exception.BusinessLogicException;
+import kr.co.springtricount.infra.exception.DuplicatedException;
+import kr.co.springtricount.infra.exception.NotFoundException;
+import kr.co.springtricount.infra.response.ResponseFormat;
+import kr.co.springtricount.infra.response.ResponseStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AuthenticationException.class)
+    protected ResponseEntity<ResponseFormat<Void>> handleAuthenticationException(AuthenticationException e) {
+        log.warn("AUTHENTICATION REQUEST: ", e);
+
+        ResponseFormat<Void> responseFormat =
+                ResponseFormat.failureMessage(ResponseStatus.FAIL_UNAUTHORIZED, e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseFormat);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    protected ResponseEntity<ResponseFormat<Void>> handleNotFoundException(NotFoundException e) {
+        log.warn("NOT FOUND REQUEST: ", e);
+
+        ResponseFormat<Void> responseFormat =
+                ResponseFormat.failureMessage(ResponseStatus.FAIL_NOT_FOUND, e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseFormat);
+    }
+
+    @ExceptionHandler(DuplicatedException.class)
+    protected ResponseEntity<ResponseFormat<Void>> handleDuplicatedException(DuplicatedException e) {
+        log.warn("DUPLICATED REQUEST: ", e);
+
+        ResponseFormat<Void> responseFormat =
+                ResponseFormat.failureMessage(ResponseStatus.FAIL_DUPLICATED, e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(responseFormat);
+    }
+
+    @ExceptionHandler(BusinessLogicException.class)
+    protected ResponseEntity<ResponseFormat<Void>> handleBusinessLogicException(BusinessLogicException e) {
+        log.warn("RUNTIME EXCEPTION: ", e);
+
+        ResponseFormat<Void> responseFormat =
+                ResponseFormat.failureMessage(ResponseStatus.FAIL_SERVER_ERROR, e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseFormat);
+    }
+}
