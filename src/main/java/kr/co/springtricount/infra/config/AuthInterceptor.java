4c74fedfef6e9acbd688b2766aaad5677d9f8f1b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-22 00:16:07 +0900

    feat: 인터셉터 구현

---
+++ src/main/java/kr/co/springtricount/infra/config/AuthInterceptor.java
@@ -0,0 +1,19 @@
+package kr.co.springtricount.infra.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (request.getSession().getAttribute("member") == null) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "로그인이 필요합니다.");
+            return false;
+        }
+        return true;
+    }
+}
