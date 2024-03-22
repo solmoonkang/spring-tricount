c325e1ace6070bf1e636af785afee16c568b4e5d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-22 18:43:10 +0900

    feat: 정산 엔티티 구현

---
+++ src/main/java/kr/co/springtricount/persistence/entity/Settlement.java
@@ -0,0 +1,34 @@
+package kr.co.springtricount.persistence.entity;
+
+import jakarta.persistence.*;
+import kr.co.springtricount.persistence.BaseEntity;
+import kr.co.springtricount.service.dto.request.SettlementReqDTO;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "tbl_settlements")
+@AttributeOverride(
+        name = "id",
+        column = @Column(name = "settlement_id", length = 4)
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Settlement extends BaseEntity {
+
+    private String name;
+
+    @Builder
+    public Settlement(String name) {
+        this.name = name;
+    }
+
+    public static Settlement toSettlementEntity(SettlementReqDTO create) {
+
+        return Settlement.builder()
+                .name(create.name())
+                .build();
+    }
+}
