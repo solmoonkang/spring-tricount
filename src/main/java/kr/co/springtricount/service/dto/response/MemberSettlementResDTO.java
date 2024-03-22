4dced6f8d47fa11b51d781980ce76aa42b1360b7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-23 02:04:58 +0900

    chore: 회원-정산 조회 DTO 명 수정

--- src/main/java/kr/co/springtricount/service/dto/response/MemberSettlementResDTO.java
+++ src/main/java/kr/co/springtricount/service/dto/response/MemberSettlementResDTO.java
@@ -2,7 +2,7 @@ package kr.co.springtricount.service.dto.response;
 
 import java.util.List;
 
-public record MemberSettlementDTO(
+public record MemberSettlementResDTO(
         String settlementName,
         List<String> memberNames
 ) { }
