87475eb1adbdb1bb2ab22f0e2b3df93b00947a13
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-23 00:08:39 +0900

    feat: 모든 정산 내역을 정산 ID로 조회하는 로직 구현

--- src/main/java/kr/co/springtricount/persistence/repository/MemberSettlementRepository.java
+++ src/main/java/kr/co/springtricount/persistence/repository/MemberSettlementRepository.java
@@ -3,5 +3,9 @@ package kr.co.springtricount.persistence.repository;
 import kr.co.springtricount.persistence.entity.MemberSettlement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberSettlementRepository extends JpaRepository<MemberSettlement, Long> {
+
+    List<MemberSettlement> findAllBySettlementId(Long id);
 }
