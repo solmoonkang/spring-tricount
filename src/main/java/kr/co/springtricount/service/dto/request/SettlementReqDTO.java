d832d81d30e955e2c97edde100f6d6776e9ff5d1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-23 00:09:59 +0900

    refactor: 가독성을 위해 DTO 변수명 수정

--- src/main/java/kr/co/springtricount/service/dto/request/SettlementReqDTO.java
+++ src/main/java/kr/co/springtricount/service/dto/request/SettlementReqDTO.java
@@ -6,6 +6,6 @@ import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 
 public record SettlementReqDTO(
-        @NotBlank(message = "정산 이름을 입력해주세요.") String name,
-        @NotEmpty(message = "정산에 참여할 회원의 이름을 입력해주세요.") List<String> membersIdentity
+        @NotBlank(message = "정산 이름을 입력해주세요.") String settlementName,
+        @NotEmpty(message = "정산에 참여할 회원의 이름을 입력해주세요.") List<String> memberIdentities
 ) { }
