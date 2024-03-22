dd2085aba63698d71e484384044567e88c78d840
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-22 18:44:34 +0900

    feat: 정산 생성 컨트롤러 로직 구현

---
+++ src/main/java/kr/co/springtricount/controller/SettlementController.java
@@ -0,0 +1,28 @@
+package kr.co.springtricount.controller;
+
+import kr.co.springtricount.infra.response.ResponseFormat;
+import kr.co.springtricount.infra.response.ResponseStatus;
+import kr.co.springtricount.service.SettlementService;
+import kr.co.springtricount.service.dto.request.SettlementReqDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/settlements")
+public class SettlementController {
+
+    private final SettlementService settlementService;
+
+    @PostMapping
+    public ResponseFormat<Void> createSettlement(@RequestBody @Validated SettlementReqDTO create) {
+
+        settlementService.createSettlement(create);
+
+        return ResponseFormat.successMessage(ResponseStatus.SUCCESS_CREATED);
+    }
+}
