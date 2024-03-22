0f0bb88ca805d6bab2ef53f72aab671f6ec3e723
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-22 17:13:33 +0900

    feat: 회원 생성 로직을 제외한 요청은 로그인을 했을 때만 접근이 가능하도록 인터셉터 구현

--- src/main/java/kr/co/springtricount/infra/config/AuthInterceptor.java
+++ src/main/java/kr/co/springtricount/infra/config/AuthInterceptor.java
@@ -5,15 +5,37 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.io.IOException;
+
 @Component
 public class AuthInterceptor implements HandlerInterceptor {
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception {
+
+        return isMemberCreationRequest(request) || isUserLoggedIn(request, response);
+    }
+
+    private boolean isMemberCreationRequest(HttpServletRequest request) {
+
+        return request.getMethod().equalsIgnoreCase("POST") &&
+                request.getRequestURI().equalsIgnoreCase("/api/v1/members");
+    }
+
+    private boolean isUserLoggedIn(HttpServletRequest request,
+                                   HttpServletResponse response) throws IOException {
+
         if (request.getSession().getAttribute(SessionConstant.LOGIN_MEMBER) == null) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "로그인이 필요합니다.");
+            sendUnauthorizedError(response);
             return false;
         }
+
         return true;
+    }
+
+    private void sendUnauthorizedError(HttpServletResponse response) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "로그인이 필요합니다.");
     }
 }
