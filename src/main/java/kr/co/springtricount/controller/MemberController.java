e89973a24732a11e5c93d8333a85f4568a9418c3
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-23 02:27:05 +0900

    chore: 세션 부분을 서비스에서 컨트롤러로 변경

--- src/main/java/kr/co/springtricount/controller/MemberController.java
+++ src/main/java/kr/co/springtricount/controller/MemberController.java
@@ -1,6 +1,7 @@
 package kr.co.springtricount.controller;
 
 import jakarta.servlet.http.HttpSession;
+import kr.co.springtricount.infra.config.SessionConstant;
 import kr.co.springtricount.infra.response.ResponseFormat;
 import kr.co.springtricount.infra.response.ResponseStatus;
 import kr.co.springtricount.service.MemberService;
@@ -50,7 +51,9 @@ public class MemberController {
     public ResponseFormat<Void> deleteMember(HttpSession httpSession,
                                              @RequestBody @Validated LoginDTO loginDTO) {
 
-        memberService.deleteMember(httpSession, loginDTO);
+        String loggedInUserIdentity = (String) httpSession.getAttribute(SessionConstant.LOGIN_MEMBER);
+
+        memberService.deleteMember(loggedInUserIdentity, loginDTO);
 
         return ResponseFormat.successMessage(ResponseStatus.SUCCESS_EXECUTE);
     }
