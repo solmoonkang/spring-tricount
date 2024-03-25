86b47aa50eb5786e6c660f5eaa0bb44131fe7dc5
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-25 18:55:45 +0900

    feat: 지출 내역 생성 DTO 구현

--- src/main/java/kr/co/springtricount/service/dto/request/ExpenseReqDTO.java
+++ src/main/java/kr/co/springtricount/service/dto/request/ExpenseReqDTO.java
@@ -8,7 +8,8 @@ import java.time.LocalDateTime;
 
 public record ExpenseReqDTO(
         @NotBlank(message = "지출 이름을 입력해주세요.") String name,
-        @NotBlank(message = "지출한 사람을 입력해주세요.") String memberName,
+        @NotBlank(message = "지출한 사람의 아이디를 입력해주세요.") String memberIdentity,
+        @NotNull(message = "참여한 정산의 아이디를 입력해주세요.") Long settlementId,
         @NotNull(message = "지출 금액을 입력해주세요.") BigDecimal amount,
         @NotBlank(message = "지출 날짜를 입력해주세요.") LocalDateTime expenseDate
 ) { }
