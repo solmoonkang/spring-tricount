7fc96628635a09c5340909e404889f070aca5288
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-25 18:56:08 +0900

    feat: 지출 내역 생성 서비스 로직 구현

--- src/main/java/kr/co/springtricount/service/ExpenseService.java
+++ src/main/java/kr/co/springtricount/service/ExpenseService.java
@@ -1,16 +1,21 @@
 package kr.co.springtricount.service;
 
-import kr.co.springtricount.infra.exception.InvalidRequestException;
 import kr.co.springtricount.infra.exception.NotFoundException;
 import kr.co.springtricount.infra.response.ResponseStatus;
 import kr.co.springtricount.persistence.entity.Expense;
+import kr.co.springtricount.persistence.entity.Member;
 import kr.co.springtricount.persistence.entity.MemberSettlement;
+import kr.co.springtricount.persistence.entity.Settlement;
 import kr.co.springtricount.persistence.repository.ExpenseRepository;
+import kr.co.springtricount.persistence.repository.MemberRepository;
 import kr.co.springtricount.persistence.repository.MemberSettlementRepository;
+import kr.co.springtricount.persistence.repository.SettlementRepository;
 import kr.co.springtricount.service.dto.request.ExpenseReqDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,32 +24,35 @@ public class ExpenseService {
 
     private final ExpenseRepository expenseRepository;
 
+    private final MemberRepository memberRepository;
+
+    private final SettlementRepository settlementRepository;
+
     private final MemberSettlementRepository memberSettlementRepository;
 
     @Transactional
-    public void createExpense(ExpenseReqDTO create, String memberLoginIdentity, Long settlementId) {
+    public void createExpense(ExpenseReqDTO create, String memberLoginIdentity) {
 
-        final MemberSettlement memberSettlement = memberSettlementRepository
-                .findByMemberIdentityAndSettlementId(memberLoginIdentity, settlementId)
-                .stream().findFirst()
-                .orElseThrow(() -> new NotFoundException(ResponseStatus.FAIL_NOT_FOUND));
+        checkMemberParticipationInSettlements(memberLoginIdentity);
 
-        checkMemberNameMatchesSettlementParticipant(memberSettlement, create.memberName());
+        final Settlement settlement = settlementRepository.findById(create.settlementId())
+                .orElseThrow(() -> new NotFoundException(ResponseStatus.FAIL_SETTLEMENT_NOT_FOUND));
 
-        final Expense expense = Expense.toExpenseEntity(
-                create,
-                memberSettlement.getMember(),
-                memberSettlement.getSettlement()
-        );
+        final Member member = memberRepository.findMemberByIdentity(create.memberIdentity())
+                .orElseThrow(() -> new NotFoundException("입력한 회원 아이디는 정산에 참여한 회원 아이디와 일치하지 않습니다."));
+
+        final Expense expense = Expense.toExpenseEntity(create, member, settlement);
 
         expenseRepository.save(expense);
     }
 
-    private void checkMemberNameMatchesSettlementParticipant(MemberSettlement memberSettlement,
-                                                             String memberName) {
+    private void checkMemberParticipationInSettlements(String memberLoginIdentity) {
 
-        if (!memberSettlement.getMember().getName().equals(memberName)) {
-            throw new InvalidRequestException("입력한 회원 이름이 정산에 참여한 회원의 이름과 일치하지 않습니다.");
+        List<MemberSettlement> memberSettlements =
+                memberSettlementRepository.findAllByMemberIdentity(memberLoginIdentity);
+
+        if (memberSettlements.isEmpty()) {
+            throw new NotFoundException("참여한 정산이 없어서 지출 내역을 생성할 수 없습니다.");
         }
     }
 }
