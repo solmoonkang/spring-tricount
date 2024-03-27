54285a13a3ae1fd05f1aeb1f665fc6b7739106b2
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-27 18:34:30 +0900

    chore: LocalDateTime 타입을 LocalDate 타입으로 수정

--- src/main/java/kr/co/springtricount/service/dto/request/ExpenseReqDTO.java
+++ src/main/java/kr/co/springtricount/service/dto/request/ExpenseReqDTO.java
@@ -2,14 +2,15 @@ package kr.co.springtricount.service.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public record ExpenseReqDTO(
         @NotBlank(message = "지출 이름을 입력해주세요.") String name,
         @NotBlank(message = "지출한 사람의 아이디를 입력해주세요.") String memberIdentity,
         @NotNull(message = "참여한 정산의 아이디를 입력해주세요.") Long settlementId,
         @NotNull(message = "지출 금액을 입력해주세요.") BigDecimal amount,
-        @NotBlank(message = "지출 날짜를 입력해주세요.") LocalDateTime expenseDate
+        @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate expenseDate
 ) { }
