8171ebc904b70bbd3ce04fe18cfbb39a670d2354
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-27 22:27:17 +0900

    feat: 금액의 최소 값으로 0을 입력받을 수 있도록 구현

--- src/main/java/kr/co/springtricount/service/dto/request/ExpenseReqDTO.java
+++ src/main/java/kr/co/springtricount/service/dto/request/ExpenseReqDTO.java
@@ -1,7 +1,7 @@
 package kr.co.springtricount.service.dto.request;
 
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
@@ -10,7 +10,6 @@ import java.time.LocalDate;
 public record ExpenseReqDTO(
         @NotBlank(message = "지출 이름을 입력해주세요.") String name,
         @NotBlank(message = "지출한 사람의 아이디를 입력해주세요.") String memberIdentity,
-        @NotNull(message = "참여한 정산의 아이디를 입력해주세요.") Long settlementId,
-        @NotNull(message = "지출 금액을 입력해주세요.") BigDecimal amount,
+        @DecimalMin(value = "0") BigDecimal amount,
         @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate expenseDate
 ) { }
