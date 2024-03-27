1ee9411b6d8383793cd7878935aa1cd38295ebbb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-27 18:36:53 +0900

    chore: URI 수정

--- src/main/java/kr/co/springtricount/controller/ExpenseController.java
+++ src/main/java/kr/co/springtricount/controller/ExpenseController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/expense")
+@RequestMapping("/api/v1/expenses")
 public class ExpenseController {
 
     private final ExpenseService expenseService;
