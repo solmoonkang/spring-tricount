3d048cb12aaf90e85fc7801ab7940e27e60bb6f0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-31 18:47:00 +0900

    feat: 정산 결과 엔티티 구현

--- src/main/java/kr/co/springtricount/persistence/entity/Balance.java
+++ src/main/java/kr/co/springtricount/persistence/entity/Balance.java
@@ -1,4 +1,33 @@
 package kr.co.springtricount.persistence.entity;
 
-public class Balance {
+import jakarta.persistence.*;
+import kr.co.springtricount.persistence.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tbl_balances")
+@AttributeOverride(
+        name = "id",
+        column = @Column(name = "balance_id", length = 4)
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Balance extends BaseEntity {
+
+    @OneToOne
+    @JoinColumn(name = "settlement_id")
+    private Settlement settlement;
+
+    @Column(name = "total_amount")
+    private BigDecimal totalAmount;
+
+    @Builder
+    public Balance(Settlement settlement,
+                   BigDecimal totalAmount) {
+        this.settlement = settlement;
+        this.totalAmount = totalAmount;
+    }
 }
