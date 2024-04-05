b4b872d00c62e9125dd5b1a0f71ede8cf264a00a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-05 18:06:27 +0900

    chore: 지출 요청 DTO 명 수정

--- src/main/java/kr/co/springtricount/persistence/entity/Expense.java
+++ src/main/java/kr/co/springtricount/persistence/entity/Expense.java
@@ -2,7 +2,7 @@ package kr.co.springtricount.persistence.entity;
 
 import jakarta.persistence.*;
 import kr.co.springtricount.persistence.BaseEntity;
-import kr.co.springtricount.service.dto.response.ExpenseResDTO;
+import kr.co.springtricount.service.dto.request.ExpenseReqDTO;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -52,16 +52,16 @@ public class Expense extends BaseEntity {
         this.expenseDate = expenseDate;
     }
 
-    public static Expense toExpenseEntity(ExpenseResDTO create,
+    public static Expense toExpenseEntity(ExpenseReqDTO expenseReqDTO,
                                           Member member,
                                           Settlement settlement) {
 
         return Expense.builder()
-                .name(create.name())
+                .name(expenseReqDTO.expenseName())
                 .member(member)
                 .settlement(settlement)
-                .amount(create.amount())
-                .expenseDate(create.expenseDate())
+                .amount(expenseReqDTO.amount())
+                .expenseDate(expenseReqDTO.expenseDate())
                 .build();
     }
 }
