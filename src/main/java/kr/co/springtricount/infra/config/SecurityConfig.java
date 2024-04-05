f4d85fe1252e9efe2d8d68aa34e377f22e62e877
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-05 16:02:10 +0900

    feat: 스프링 시큐리티 설정 구현

---
+++ src/main/java/kr/co/springtricount/infra/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package kr.co.springtricount.infra.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeHttpRequests()
+                .requestMatchers("/api/v1/members/**").authenticated();
+
+        httpSecurity.formLogin().permitAll()
+                .defaultSuccessUrl("/api/v1/members");
+
+        httpSecurity.logout()
+                .logoutSuccessUrl("/login")
+                .logoutSuccessHandler(logoutSuccessHandler())
+                .deleteCookies("remember-me");
+
+        return httpSecurity.build();
+    }
+
+    private LogoutSuccessHandler logoutSuccessHandler() {
+
+        return ((request, response, authentication) -> {
+            response.sendRedirect("/login");
+        });
+    }
+}
