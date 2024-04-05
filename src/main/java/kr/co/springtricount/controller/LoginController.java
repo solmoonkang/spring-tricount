e86f038075bbef925438d64f4ad5fc23f63705de
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-05 11:43:54 +0900

    chore: 로그인, 로그아웃 로직 수정

---
+++ src/main/java/kr/co/springtricount/controller/LoginController.java
@@ -0,0 +1,50 @@
+package kr.co.springtricount.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import kr.co.springtricount.annotation.Login;
+import kr.co.springtricount.infra.response.ResponseFormat;
+import kr.co.springtricount.infra.response.ResponseStatus;
+import kr.co.springtricount.service.LoginService;
+import kr.co.springtricount.service.dto.request.LoginReqDTO;
+import kr.co.springtricount.service.dto.request.MemberReqDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static kr.co.springtricount.infra.config.SessionConstant.LOGIN_MEMBER;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/")
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @PostMapping("/login")
+    public ResponseFormat<MemberReqDTO> login(HttpServletRequest request,
+                                      @Validated @RequestBody LoginReqDTO login) {
+
+        loginService.login(login.identity(), login.password());
+
+        HttpSession httpSession = request.getSession();
+
+        httpSession.getAttribute(LOGIN_MEMBER);
+
+        return ResponseFormat.successMessage(ResponseStatus.SUCCESS_EXECUTE);
+    }
+
+    @PostMapping("/logout")
+    public ResponseFormat<Void> logout(HttpServletRequest request,
+                                       @Login MemberReqDTO member) {
+
+        HttpSession httpSession = request.getSession(false);
+
+        httpSession.invalidate();
+
+        return ResponseFormat.successMessage(ResponseStatus.SUCCESS_EXECUTE);
+    }
+}
