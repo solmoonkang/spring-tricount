df21558e9b5d9227448456fb73b8faf615c56bbe
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-05 16:51:30 +0900

    feat: 정산 요청 DTO 구현

--- src/main/java/kr/co/springtricount/service/dto/request/SettlementReqDTO.java
+++ src/main/java/kr/co/springtricount/service/dto/request/SettlementReqDTO.java
@@ -1,4 +1,7 @@
 package kr.co.springtricount.service.dto.request;
 
-public record SettlementReqDTO() {
-}
+import jakarta.validation.constraints.NotBlank;
+
+public record SettlementReqDTO(
+        @NotBlank(message = "정산 이름을 입력해주세요.") String settlementName
+) { }
