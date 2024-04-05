67f7ca2d7cab016f658d5a98ceeaa1cf67ec6031
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-05 11:23:06 +0900

    feat: 회원가입 DTO 구현

---
+++ src/main/java/kr/co/springtricount/service/dto/request/SignupReqDTO.java
@@ -0,0 +1,9 @@
+package kr.co.springtricount.service.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+
+public record SignupReqDTO(
+        @NotNull(message = "아이디를 입력해주세요.") String identity,
+        @NotNull(message = "이름을 입력해주세요.") String name,
+        @NotNull(message = "비밀번호를 입력해주세요.") String password
+) { }
