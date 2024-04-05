11f4cbc111eb4722123c1601f5bcc787d8c9a329
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-05 11:10:38 +0900

    chore: member 상수 명 변경

--- src/main/java/kr/co/springtricount/infra/config/SessionConstant.java
+++ src/main/java/kr/co/springtricount/infra/config/SessionConstant.java
@@ -2,7 +2,7 @@ package kr.co.springtricount.infra.config;
 
 public final class SessionConstant {
 
-    public static final String LOGIN_MEMBER = "member";
+    public static final String LOGIN_MEMBER = "loginMember";
 
     private SessionConstant() {
     }
