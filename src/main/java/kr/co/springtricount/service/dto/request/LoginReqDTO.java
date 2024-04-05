d43cdfd94cc809b3876ce26a92c23f43127f3caf
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-05 11:22:55 +0900

    chore: 검증 어노테이션 수정

--- src/main/java/kr/co/springtricount/service/dto/request/LoginReqDTO.java
+++ src/main/java/kr/co/springtricount/service/dto/request/LoginReqDTO.java
@@ -1,8 +1,8 @@
 package kr.co.springtricount.service.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record LoginReqDTO(
-        @NotBlank(message = "아이디를 입력해주세요.") String identity,
-        @NotBlank(message = "비밀번호를 입력해주세요.") String password
+        @NotNull(message = "아이디를 입력해주세요.") String identity,
+        @NotNull(message = "비밀번호를 입력해주세요.") String password
 ) { }
