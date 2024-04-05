c2688e170b5ade0babb78a0a09d63a736810e424
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-05 18:06:34 +0900

    chore: 지출 요청 DTO 명 수정

--- src/main/java/kr/co/springtricount/controller/ExpenseController.java
+++ src/main/java/kr/co/springtricount/controller/ExpenseController.java
@@ -1,12 +1,13 @@
 package kr.co.springtricount.controller;
 
-import kr.co.springtricount.annotation.Login;
 import kr.co.springtricount.infra.response.ResponseFormat;
 import kr.co.springtricount.infra.response.ResponseStatus;
+import kr.co.springtricount.service.dto.request.ExpenseReqDTO;
 import kr.co.springtricount.service.service.ExpenseService;
-import kr.co.springtricount.service.dto.response.MemberResDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
+import com.sun.security.auth.UserPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,21 +17,20 @@ public class ExpenseController {
 
     private final ExpenseService expenseService;
 
-    @PostMapping("/{settlement_id}")
-    public ResponseFormat<Void> createExpense(@PathVariable(name = "settlement_id") Long settlementId,
-                                              @RequestBody @Validated ExpenseReqDTO create,
-                                              @Login MemberResDTO member) {
+    @PostMapping
+    public ResponseFormat<Void> createExpense(@AuthenticationPrincipal UserPrincipal currentMember,
+                                              @RequestBody @Validated ExpenseReqDTO expenseReqDTO) {
 
-        expenseService.createExpense(settlementId, create, member.identity());
+        expenseService.createExpense(currentMember, expenseReqDTO);
 
         return ResponseFormat.successMessage(ResponseStatus.SUCCESS_CREATED);
     }
 
     @DeleteMapping("/{expense_id}")
-    public ResponseFormat<Void> deleteExpense(@PathVariable(name = "expense_id") Long expenseId,
-                                              @Login MemberResDTO member) {
+    public ResponseFormat<Void> deleteExpense(@AuthenticationPrincipal UserPrincipal currentMember,
+                                              @PathVariable(name = "expense_id") Long expenseId) {
 
-        expenseService.deleteExpenseById(expenseId, member.identity());
+        expenseService.deleteExpenseById(currentMember, expenseId);
 
         return ResponseFormat.successMessage(ResponseStatus.SUCCESS_EXECUTE);
     }
