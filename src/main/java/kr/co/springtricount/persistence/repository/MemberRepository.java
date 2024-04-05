291913052defc0ae23fa2a791151872d87f63800
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-05 16:50:54 +0900

    chore: 불필요한 로직 제거

--- src/main/java/kr/co/springtricount/persistence/repository/MemberRepository.java
+++ src/main/java/kr/co/springtricount/persistence/repository/MemberRepository.java
@@ -1,11 +1,9 @@
 package kr.co.springtricount.persistence.repository;
 
 import kr.co.springtricount.persistence.entity.Member;
-import kr.co.springtricount.service.dto.response.MemberResDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,6 +12,4 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsMemberByIdentity(String identity);
 
     Optional<Member> findMemberByIdentity(String identity);
-
-    List<MemberResDTO> findAllByIdentityIn(List<MemberResDTO> participants);
 }
