d67c14f5c598daede98e23fb4178fe8e46acc0b0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-05 16:29:43 +0900

    chore: 인터셉터, 인터셉터 설정 제거

--- src/main/java/kr/co/springtricount/infra/config/SessionConstant.java
+++
@@ -1,9 +0,0 @@
-package kr.co.springtricount.infra.config;
-
-public final class SessionConstant {
-
-    public static final String LOGIN_MEMBER = "loginMember";
-
-    private SessionConstant() {
-    }
-}
