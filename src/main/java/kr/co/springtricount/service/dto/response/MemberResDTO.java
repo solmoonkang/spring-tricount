9fbd6809ef678bb462681dd915ac4b85f4c87685
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-05 12:43:36 +0900

    chore: 불필요한 클래스 제거

--- src/main/java/kr/co/springtricount/service/dto/response/MemberResDTO.java
+++
@@ -1,6 +0,0 @@
-package kr.co.springtricount.service.dto.response;
-
-public record MemberResDTO(
-        String identity,
-        String name
-) { }
--- src/main/java/kr/co/springtricount/service/dto/response/SettlementResDTO.java
+++
@@ -1,9 +0,0 @@
-package kr.co.springtricount.service.dto.response;
-
-import java.util.List;
-
-public record SettlementResDTO(
-        String settlementName,
-        List<String> memberNames,
-        List<ExpenseResDTO> expenseList
-) { }
