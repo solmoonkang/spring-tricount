4d5d06ebfa0d50461dc6cf3fc4dab6953494e093
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-05 11:47:58 +0900

    chore: HttpSession을 @Login 어노테이션으로 수정

--- src/main/java/kr/co/springtricount/controller/ExpenseController.java
+++ src/main/java/kr/co/springtricount/controller/ExpenseController.java
@@ -1,11 +1,13 @@
 package kr.co.springtricount.controller;
 
 import jakarta.servlet.http.HttpSession;
+import kr.co.springtricount.annotation.Login;
 import kr.co.springtricount.infra.config.SessionConstant;
 import kr.co.springtricount.infra.response.ResponseFormat;
 import kr.co.springtricount.infra.response.ResponseStatus;
 import kr.co.springtricount.service.ExpenseService;
 import kr.co.springtricount.service.dto.request.ExpenseReqDTO;
+import kr.co.springtricount.service.dto.request.MemberReqDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -20,22 +22,18 @@ public class ExpenseController {
     @PostMapping("/{settlement_id}")
     public ResponseFormat<Void> createExpense(@PathVariable(name = "settlement_id") Long settlementId,
                                               @RequestBody @Validated ExpenseReqDTO create,
-                                              HttpSession httpSession) {
+                                              @Login MemberReqDTO member) {
 
-        final String memberLoginIdentity = (String) httpSession.getAttribute(SessionConstant.LOGIN_MEMBER);
-
-        expenseService.createExpense(settlementId, create, memberLoginIdentity);
+        expenseService.createExpense(settlementId, create, member.identity());
 
         return ResponseFormat.successMessage(ResponseStatus.SUCCESS_CREATED);
     }
 
     @DeleteMapping("/{expense_id}")
     public ResponseFormat<Void> deleteExpense(@PathVariable(name = "expense_id") Long expenseId,
-                                              HttpSession httpSession) {
+                                              @Login MemberReqDTO member) {
 
-        final String memberLoginIdentity = (String) httpSession.getAttribute(SessionConstant.LOGIN_MEMBER);
-
-        expenseService.deleteExpenseById(expenseId, memberLoginIdentity);
+        expenseService.deleteExpenseById(expenseId, member.identity());
 
         return ResponseFormat.successMessage(ResponseStatus.SUCCESS_EXECUTE);
     }
