e390456f235a42a16969947a76a4e12adc853f24
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-05 11:43:15 +0900

    chore: 비밀번호는 무시하도록 수정

--- src/main/java/kr/co/springtricount/service/dto/request/MemberReqDTO.java
+++ src/main/java/kr/co/springtricount/service/dto/request/MemberReqDTO.java
@@ -1,9 +1,10 @@
 package kr.co.springtricount.service.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.NotBlank;
 
 public record MemberReqDTO(
         @NotBlank(message = "아이디를 입력해주세요.") String identity,
-        @NotBlank(message = "비밀번호를 입력해주세요.") String password,
-        @NotBlank(message = "이름을 입력해주세요.") String name
+        @NotBlank(message = "이름을 입력해주세요.") String name,
+        @JsonIgnore String password
 ) { }
