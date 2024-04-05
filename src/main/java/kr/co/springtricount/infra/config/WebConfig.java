42de6c6cb2684508a38b1546039efa60577b17ee
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-05 11:19:27 +0900

    feat: 리졸버 구현

--- src/main/java/kr/co/springtricount/infra/config/WebConfig.java
+++ src/main/java/kr/co/springtricount/infra/config/WebConfig.java
@@ -2,19 +2,32 @@ package kr.co.springtricount.infra.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    private final AuthInterceptor authInterceptor;
+    private final LoginCheckInterceptor loginCheckInterceptor;
+
+    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authInterceptor)
-                .addPathPatterns("/api/v1/**")
-                .excludePathPatterns("/api/v1/login", "/api/v1/logout");
+
+        registry.addInterceptor(loginCheckInterceptor)
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/api/v1/signup", "/api/v1/login", "/h2-console/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+
+        resolvers.add(loginMemberArgumentResolver);
     }
 }
