0a673e33d1813054457afd6edc6bbe350194531c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-05 12:48:03 +0900

    chore: 정산 조회 DTO 명 수정

--- src/main/java/kr/co/springtricount/controller/SettlementController.java
+++ src/main/java/kr/co/springtricount/controller/SettlementController.java
@@ -4,9 +4,8 @@ import kr.co.springtricount.annotation.Login;
 import kr.co.springtricount.infra.response.ResponseFormat;
 import kr.co.springtricount.infra.response.ResponseStatus;
 import kr.co.springtricount.service.SettlementService;
-import kr.co.springtricount.service.dto.request.MemberReqDTO;
-import kr.co.springtricount.service.dto.request.SettlementReqDTO;
-import kr.co.springtricount.service.dto.response.SettlementResDTO;
+import kr.co.springtricount.service.dto.MemberDTO;
+import kr.co.springtricount.service.dto.SettlementDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +20,7 @@ public class SettlementController {
     private final SettlementService settlementService;
 
     @PostMapping
-    public ResponseFormat<Void> createSettlement(@RequestBody @Validated SettlementReqDTO create) {
+    public ResponseFormat<Void> createSettlement(@RequestBody @Validated SettlementDTO create) {
 
         settlementService.createSettlement(create);
 
@@ -29,7 +28,7 @@ public class SettlementController {
     }
 
     @GetMapping
-    public ResponseFormat<List<SettlementResDTO>> findAllSettlementsByMember(@Login MemberReqDTO member) {
+    public ResponseFormat<List<SettlementDTO>> findAllSettlementsByMember(@Login MemberDTO member) {
 
         return ResponseFormat.successMessageWithData(
                 ResponseStatus.SUCCESS_EXECUTE,
@@ -39,7 +38,7 @@ public class SettlementController {
 
     @DeleteMapping("/{settlement_id}")
     public ResponseFormat<Void> deleteSettlementById(@PathVariable(name = "settlement_id") Long settlementId,
-                                                     @Login MemberReqDTO member) {
+                                                     @Login MemberDTO member) {
 
 
         settlementService.deleteSettlementById(settlementId, member.identity());
