96cbfb54620755282601099b5a02ffd55012fe9a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-11 14:48:38 +0900

    feat: 정산 결과 조회 시 발생하는 N+1 문제 해결

---
+++ src/main/java/kr/co/springtricount/persistence/repository/search/BalanceSearchRepository.java
@@ -0,0 +1,50 @@
+package kr.co.springtricount.persistence.repository.search;
+
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import kr.co.springtricount.persistence.entity.QExpense;
+import kr.co.springtricount.persistence.entity.QMember;
+import kr.co.springtricount.persistence.entity.QMemberSettlement;
+import kr.co.springtricount.service.dto.response.MemberResDTO;
+import kr.co.springtricount.service.service.BalanceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+@RequiredArgsConstructor
+public class BalanceSearchRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    private final QMember member = QMember.member;
+
+    private final QMemberSettlement memberSettlement = QMemberSettlement.memberSettlement;
+
+    private final QExpense expense = QExpense.expense;
+
+    public List<BalanceService.Balance> findBalancesBySettlementId(Long settlementId) {
+
+        List<Tuple> results = jpaQueryFactory
+                .select(member.id, member.identity, member.name, expense.amount.sum())
+                .from(memberSettlement)
+                .join(memberSettlement.member, member)
+                .leftJoin(expense).on(expense.settlement.id.eq(memberSettlement.settlement.id))
+                .where(memberSettlement.settlement.id.eq(settlementId))
+                .groupBy(member.id)
+                .fetch();
+
+        // 결과 처리를 단순화
+        return results.stream()
+                .map(tuple -> new BalanceService.Balance(
+                        new MemberResDTO(
+                                tuple.get(member.id),
+                                tuple.get(member.identity),
+                                tuple.get(member.name)),
+                        tuple.get(expense.amount.sum())
+                ))
+                .collect(Collectors.toList());
+    }
+}
