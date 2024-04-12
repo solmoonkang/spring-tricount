b1232a2ea42897f808b014951db9d416725638bf
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-12 18:36:44 +0900

    feat: 모든 채팅방 조회 서비스 로직 구현

--- src/main/java/kr/co/springtricount/service/service/ChatService.java
+++ src/main/java/kr/co/springtricount/service/service/ChatService.java
@@ -10,10 +10,13 @@ import kr.co.springtricount.persistence.repository.ChatRoomRepository;
 import kr.co.springtricount.persistence.repository.MemberRepository;
 import kr.co.springtricount.service.dto.request.ChatMessageReqDTO;
 import kr.co.springtricount.service.dto.request.ChatRoomReqDTO;
+import kr.co.springtricount.service.dto.response.ChatRoomResDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -40,12 +43,6 @@ public class ChatService {
         chatRoomRepository.save(chatRoom);
     }
 
-    /**
-     * createChatMessage: 채팅 메시지 생성 로직
-     * 채팅 메시지 전송 시 채팅방 안에서 전송이 이루어진다. 따라서 채팅 메시지는 채팅방 ID를 가지고 있다.
-     * 만약, 채팅 메시지가 갖는 채팅방 ID가 이미 생성된 채팅방이라면 그냥 메시지를 전송하면 된다.
-     * 반대로, 채팅 메시지를 보낼 때 채팅방 ID가 없는 채팅방이라면 새로운 채팅방을 생성하고, 메시지를 전송한다.
-     */
     @Transactional
     public void createChatMessage(User currentMember, Long chatRoomId, ChatMessageReqDTO chatMessageReqDTO) {
 
@@ -60,6 +57,15 @@ public class ChatService {
         final ChatMessage chatMessage = ChatMessage.toChatMessageEntity(sender, chatRoom, chatMessageReqDTO);
 
         chatMessageRepository.save(chatMessage);
+    }
+
+    public List<ChatRoomResDTO> findAllChatRooms() {
+
+        final List<ChatRoom> chatRooms = chatRoomRepository.findAll();
+
+        return chatRooms.stream()
+                .map(chatRoom -> new ChatRoomResDTO(chatRoom.getName()))
+                .toList();
     }
 
     private ChatRoom createOrGetChatRoom(Long chatRoomId, Member receiver) {
