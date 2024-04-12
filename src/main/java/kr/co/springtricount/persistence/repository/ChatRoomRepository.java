eb74ba719fdc6dd43ad6212056abe49a813421db
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-12 19:35:01 +0900

    feat: 로그인 한 사용자로 모든 채팅방 목록 조회하는 로직 구현

--- src/main/java/kr/co/springtricount/persistence/repository/ChatRoomRepository.java
+++ src/main/java/kr/co/springtricount/persistence/repository/ChatRoomRepository.java
@@ -1,7 +1,12 @@
 package kr.co.springtricount.persistence.repository;
 
 import kr.co.springtricount.persistence.entity.chat.ChatRoom;
+import kr.co.springtricount.persistence.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    List<ChatRoom> findAllByMember(Member member);
 }
