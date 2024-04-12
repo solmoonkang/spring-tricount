7464c627dc35753d09daf068243072b2730e9a2f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-12 17:32:17 +0900

    feat: 채팅방 생성 서비스 로직 구현

--- src/main/java/kr/co/springtricount/service/service/ChatService.java
+++ src/main/java/kr/co/springtricount/service/service/ChatService.java
@@ -2,17 +2,13 @@ package kr.co.springtricount.service.service;
 
 import kr.co.springtricount.infra.exception.NotFoundException;
 import kr.co.springtricount.infra.response.ResponseStatus;
-import kr.co.springtricount.persistence.entity.chat.ChatMessage;
 import kr.co.springtricount.persistence.entity.chat.ChatRoom;
-import kr.co.springtricount.persistence.entity.chat.MessageType;
 import kr.co.springtricount.persistence.entity.member.Member;
 import kr.co.springtricount.persistence.repository.ChatMessageRepository;
 import kr.co.springtricount.persistence.repository.ChatRoomRepository;
 import kr.co.springtricount.persistence.repository.MemberRepository;
-import kr.co.springtricount.service.dto.request.ChatMessageReqDTO;
 import kr.co.springtricount.service.dto.request.ChatRoomReqDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,36 +24,23 @@ public class ChatService {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public void createChatRoom(User currentMember, ChatRoomReqDTO chatRoomReqDTO) {
+    public void createChatRoom(ChatRoomReqDTO chatRoomReqDTO) {
 
-        final Member findMember = memberRepository.findMemberByIdentity(currentMember.getUsername())
+        final Member receiver = memberRepository.findMemberByIdentity(chatRoomReqDTO.receiverIdentity())
                 .orElseThrow(() -> new NotFoundException(ResponseStatus.FAIL_MEMBER_NOT_FOUND));
 
-        final ChatRoom chatRoom = ChatRoom.builder()
-                .name(chatRoomReqDTO.chatRoomName())
-                .messageType(MessageType.ENTER)
-                .member(findMember)
-                .chatMessages(null)
-                .build();
+        final ChatRoom chatRoom =
+                ChatRoom.toChatRoomEntity(receiver, determineChatRoomName(chatRoomReqDTO, receiver));
 
         chatRoomRepository.save(chatRoom);
     }
 
-    @Transactional
-    public void createChatMessage(Long chatRoomId, ChatMessageReqDTO chatMessageReqDTO) {
+    private String determineChatRoomName(ChatRoomReqDTO chatRoomReqDTO, Member receiver) {
 
-        final Member findMember = memberRepository.findMemberByIdentity(chatMessageReqDTO.sender())
-                .orElseThrow(() -> new NotFoundException(ResponseStatus.FAIL_MEMBER_NOT_FOUND));
+        if (!chatRoomReqDTO.chatRoomName().isBlank()) {
+            return chatRoomReqDTO.chatRoomName();
+        }
 
-        final ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
-                .orElseThrow(() -> new NotFoundException("해당 채팅방은 존재하지 않습니다."));
-
-        final ChatMessage chatMessage = ChatMessage.builder()
-                .sender(findMember)
-                .chatRoom(chatRoom)
-                .message(chatMessageReqDTO.message())
-                .build();
-
-        chatMessageRepository.save(chatMessage);
+        return receiver.getName();
     }
 }
