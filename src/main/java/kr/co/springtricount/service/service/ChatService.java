213b1591def49387ee90192afdc648eb7fe26f9f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-12 16:14:40 +0900

    feat: 채팅메시지 생성 서비스 로직 구현

--- src/main/java/kr/co/springtricount/service/service/ChatService.java
+++ src/main/java/kr/co/springtricount/service/service/ChatService.java
@@ -2,12 +2,14 @@ package kr.co.springtricount.service.service;
 
 import kr.co.springtricount.infra.exception.NotFoundException;
 import kr.co.springtricount.infra.response.ResponseStatus;
+import kr.co.springtricount.persistence.entity.chat.ChatMessage;
 import kr.co.springtricount.persistence.entity.chat.ChatRoom;
 import kr.co.springtricount.persistence.entity.chat.MessageType;
 import kr.co.springtricount.persistence.entity.member.Member;
 import kr.co.springtricount.persistence.repository.ChatMessageRepository;
 import kr.co.springtricount.persistence.repository.ChatRoomRepository;
 import kr.co.springtricount.persistence.repository.MemberRepository;
+import kr.co.springtricount.service.dto.request.ChatMessageReqDTO;
 import kr.co.springtricount.service.dto.request.ChatRoomReqDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.User;
@@ -35,8 +37,27 @@ public class ChatService {
                 .name(chatRoomReqDTO.chatRoomName())
                 .messageType(MessageType.ENTER)
                 .member(findMember)
+                .chatMessages(null)
                 .build();
 
         chatRoomRepository.save(chatRoom);
+    }
+
+    @Transactional
+    public void createChatMessage(Long chatRoomId, ChatMessageReqDTO chatMessageReqDTO) {
+
+        final Member findMember = memberRepository.findMemberByIdentity(chatMessageReqDTO.sender())
+                .orElseThrow(() -> new NotFoundException(ResponseStatus.FAIL_MEMBER_NOT_FOUND));
+
+        final ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
+                .orElseThrow(() -> new NotFoundException("해당 채팅방은 존재하지 않습니다."));
+
+        final ChatMessage chatMessage = ChatMessage.builder()
+                .sender(findMember)
+                .chatRoom(chatRoom)
+                .message(chatMessageReqDTO.message())
+                .build();
+
+        chatMessageRepository.save(chatMessage);
     }
 }
