283d66684ae0cdc8ad2f363dd2acd3e336908fd9
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-12 20:07:20 +0900

    feat: 채팅 메시지 조회 컨트롤러 로직 구현

--- src/main/java/kr/co/springtricount/controller/ChatMessageController.java
+++ src/main/java/kr/co/springtricount/controller/ChatMessageController.java
@@ -3,12 +3,15 @@ package kr.co.springtricount.controller;
 import kr.co.springtricount.infra.response.ResponseFormat;
 import kr.co.springtricount.infra.response.ResponseStatus;
 import kr.co.springtricount.service.dto.request.ChatMessageReqDTO;
+import kr.co.springtricount.service.dto.response.ChatMessageResDTO;
 import kr.co.springtricount.service.service.ChatMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +28,16 @@ public class ChatMessageController {
         chatMessageService.createChatMessage(currentMember, chatRoomId, chatMessageReqDTO);
 
         return ResponseFormat.successMessage(ResponseStatus.SUCCESS_CREATED);
+    }
+
+    @GetMapping("/{chat_room_id}")
+    public ResponseFormat<List<ChatMessageResDTO>> findAllChatMessagesByChatRoomId(
+            @AuthenticationPrincipal User currentMember,
+            @PathVariable("chat_room_id") Long chatRoomId) {
+
+        return ResponseFormat.successMessageWithData(
+                ResponseStatus.SUCCESS_EXECUTE,
+                chatMessageService.findAllChatMessagesByChatRoomId(currentMember, chatRoomId)
+        );
     }
 }
