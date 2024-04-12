266f35c68578c2f40f094595dcb3816e758a84d1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-12 18:50:31 +0900

    feat: 채팅방 ID로 모든 채팅 메시지 조회 로직 구현

--- src/main/java/kr/co/springtricount/persistence/repository/ChatMessageRepository.java
+++ src/main/java/kr/co/springtricount/persistence/repository/ChatMessageRepository.java
@@ -3,5 +3,9 @@ package kr.co.springtricount.persistence.repository;
 import kr.co.springtricount.persistence.entity.chat.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+
+    List<ChatMessage> findChatMessagesByChatRoomId(Long chatRoomId);
 }
