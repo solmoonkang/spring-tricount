ae9a93086f623f8b334eedb83bb7ad988aa89939
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-15 18:08:22 +0900

    chore: 채팅방 이름 필드명 수정

--- src/main/java/kr/co/springtricount/service/dto/request/ChatRoomReqDTO.java
+++ src/main/java/kr/co/springtricount/service/dto/request/ChatRoomReqDTO.java
@@ -1,8 +1,5 @@
 package kr.co.springtricount.service.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-
 public record ChatRoomReqDTO(
-        String chatRoomName,
-        @NotBlank String receiverIdentity
+        String name
 ) { }
