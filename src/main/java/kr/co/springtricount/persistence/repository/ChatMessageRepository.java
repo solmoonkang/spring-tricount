a9d1ba09881eb95a972c304619bee7d3978fedb8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-15 18:35:07 +0900

    feat: 채팅방 목록 조회 시 가장 최근 메시지도 같이 조회할 수 있도록 구현

--- src/main/java/kr/co/springtricount/persistence/repository/ChatMessageRepository.java
+++ src/main/java/kr/co/springtricount/persistence/repository/ChatMessageRepository.java
@@ -4,8 +4,11 @@ import kr.co.springtricount.persistence.entity.chat.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
     List<ChatMessage> findChatMessagesByChatRoomId(Long chatRoomId);
+
+    Optional<ChatMessage> findTopByChatRoomIdOrderByCreatedDateDesc(Long chatRoomId);
 }
