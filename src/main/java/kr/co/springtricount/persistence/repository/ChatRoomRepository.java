ac1d9316a9d3c9620630b4157a5ebf37880771d3
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-15 20:01:06 +0900

    feat: 채팅방 ID와 회원으로 채팅방을 조회하는 메서드 구현

--- src/main/java/kr/co/springtricount/persistence/repository/ChatRoomRepository.java
+++ src/main/java/kr/co/springtricount/persistence/repository/ChatRoomRepository.java
@@ -5,8 +5,11 @@ import kr.co.springtricount.persistence.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
-    List<ChatRoom> findAllByMember(Member member);
+    List<ChatRoom> findChatRoomByMember(Member member);
+
+    Optional<ChatRoom> findByIdAndMember(Long chatRoomId, Member member);
 }
