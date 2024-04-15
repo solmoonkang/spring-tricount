00e5327b57d9194e6082bd12f07972c5ba48bdae
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-15 21:59:39 +0900

    chore: 불필요한 로직 제거

--- src/main/java/kr/co/springtricount/persistence/repository/ChatRoomRepository.java
+++ src/main/java/kr/co/springtricount/persistence/repository/ChatRoomRepository.java
@@ -5,11 +5,8 @@ import kr.co.springtricount.persistence.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-
-    Optional<ChatRoom> findByIdAndMember(Long chatRoomId, Member member);
 
     List<ChatRoom> findBySenderOrReceiver(Member sender, Member receiver);
 }
