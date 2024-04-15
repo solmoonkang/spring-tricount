aa0c244d664fbaaeeb473a45940d7ce3ff685705
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-15 18:21:00 +0900

    feat: 채팅방 요청 DTO에서 채팅방 이름이 비어있지 않도록 검증하도록 구현

--- src/main/java/kr/co/springtricount/service/dto/request/ChatRoomReqDTO.java
+++ src/main/java/kr/co/springtricount/service/dto/request/ChatRoomReqDTO.java
@@ -1,5 +1,7 @@
 package kr.co.springtricount.service.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ChatRoomReqDTO(
-        String name
+        @NotBlank(message = "채팅방 이름을 입력해주세요.") String name
 ) { }
