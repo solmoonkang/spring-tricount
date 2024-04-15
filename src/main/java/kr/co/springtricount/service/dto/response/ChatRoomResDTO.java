13d851b8156fec2db24e3b0d1e530cb1316bf5f1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-15 17:44:24 +0900

    feat: 채팅방 응답 DTO에서 채팅 메시지도 함께 조회할 수 있도록 필드 추가

--- src/main/java/kr/co/springtricount/service/dto/response/ChatRoomResDTO.java
+++ src/main/java/kr/co/springtricount/service/dto/response/ChatRoomResDTO.java
@@ -1,5 +1,8 @@
 package kr.co.springtricount.service.dto.response;
 
+import java.util.List;
+
 public record ChatRoomResDTO(
-        String name
+        String name,
+        List<ChatMessageResDTO> chatMessages
 ) { }
