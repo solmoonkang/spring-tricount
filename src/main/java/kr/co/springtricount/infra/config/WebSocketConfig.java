673d4f42a5335c3a0cf68daf07fec2bb08589d16
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-15 17:43:58 +0900

    feat: STOMP 엔드 포인트 수정 및 버전이 낮은 브라우저에서도 적용이 가능하도록 구현

--- src/main/java/kr/co/springtricount/infra/config/WebSocketConfig.java
+++ src/main/java/kr/co/springtricount/infra/config/WebSocketConfig.java
@@ -7,6 +7,13 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+/**
+ * STOMP(Simple Text Oriented Message Protocol)
+ * 기존 WebSocket 통신 방식을 좀 더 효율적으로 조금 더 쉽게 다룰 수 있게 해주는 프로토콜이다.
+ * 해당 프로토콜은 publish, subscribe 개념으로 동작한다.
+ *  - 클라이언트가 서버로 메시지를 보내는 것을 publish(pub),
+ *  - 클라이언트가 서버로부터 메시지를 받는 것을 메시지를 구독한다는 개념으로 subscribe(sub)이 사용된다.
+ */
 @Configuration
 @EnableWebSocket
 @EnableWebSocketMessageBroker
@@ -15,15 +22,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
 
-        registry.enableSimpleBroker("/sub");
-        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/sub");                    // 해당 주소를 구독하고 있는 클라이언트에게 메시지 전달
+        registry.setApplicationDestinationPrefixes("/pub");     // 클라이언트에서 보낸 메시지를 받을 prefix
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
-                .withSockJS();
+        registry.addEndpoint("/ws-stomp")   // SockJS 연결 주소
+                .withSockJS();              // 버전이 낮은 브라우저에서도 적용이 가능
+
+        // 주소: "ws://localhost:8080/ws-stomp"
     }
 }
