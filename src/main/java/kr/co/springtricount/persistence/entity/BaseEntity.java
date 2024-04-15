3e96c051846ee7a449b5b32c010e81b3f6803720
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-15 19:57:30 +0900

    feat: 엔티티가 생성될 때 자동으로 날짜와 시간이 할당되도록 구현

--- src/main/java/kr/co/springtricount/persistence/entity/BaseEntity.java
+++ src/main/java/kr/co/springtricount/persistence/entity/BaseEntity.java
@@ -2,6 +2,7 @@ package kr.co.springtricount.persistence.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -14,6 +15,7 @@ public abstract class BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "created_date")
+    @CreatedDate
+    @Column(name = "created_date", updatable = false)
     private LocalDateTime createdDate;
 }
