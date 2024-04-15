80490502fbdceecdb7afbaab20b9e7f7a6cef63e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-15 20:46:52 +0900

    feat: 채팅을 보낼 사용자의 아이디를 입력하는 필드 추가

--- src/main/java/kr/co/springtricount/service/dto/request/ChatRoomReqDTO.java
+++ src/main/java/kr/co/springtricount/service/dto/request/ChatRoomReqDTO.java
@@ -3,5 +3,6 @@ package kr.co.springtricount.service.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record ChatRoomReqDTO(
-        @NotBlank(message = "채팅방 이름을 입력해주세요.") String name
+        @NotBlank(message = "채팅방 이름을 입력해주세요.") String name,
+        @NotBlank(message = "채팅을 할 사용자의 아이디를 입력해주세요.") String receiverIdentity
 ) { }
