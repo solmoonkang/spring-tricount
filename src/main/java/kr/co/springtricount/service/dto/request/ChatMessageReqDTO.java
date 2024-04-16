1c30536d73555b48a7f30c26ac0d84edc23e2ea4
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-16 15:30:05 +0900

    chore: 채팅 메시지 생성 시 받는 사람의 아이디 필드 제거

--- src/main/java/kr/co/springtricount/service/dto/request/ChatMessageReqDTO.java
+++ src/main/java/kr/co/springtricount/service/dto/request/ChatMessageReqDTO.java
@@ -1,8 +1,5 @@
 package kr.co.springtricount.service.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-
 public record ChatMessageReqDTO(
-        String message,
-        @NotBlank String receiverIdentity
+        String message
 ) { }
