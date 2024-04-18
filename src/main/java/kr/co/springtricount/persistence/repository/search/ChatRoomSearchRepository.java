96f332ba7fb8a1b97ff8d29e529d65ed107ce2c7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-18 15:14:27 +0900

    feat: 채팅방 목록 조회 시 발생하는 N+1 문제 해결

---
+++ src/main/java/kr/co/springtricount/persistence/repository/search/ChatRoomSearchRepository.java
@@ -0,0 +1,41 @@
+package kr.co.springtricount.persistence.repository.search;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import kr.co.springtricount.persistence.entity.chat.ChatRoom;
+import kr.co.springtricount.persistence.entity.chat.QChatMessage;
+import kr.co.springtricount.persistence.entity.chat.QChatRoom;
+import kr.co.springtricount.persistence.entity.member.Member;
+import kr.co.springtricount.persistence.entity.member.QMember;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ChatRoomSearchRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    private final QChatRoom chatRoom = QChatRoom.chatRoom;
+
+    private final QMember sender = new QMember("sender");
+
+    private final QMember receiver = new QMember("receiver");
+
+    private final QChatMessage chatMessage = QChatMessage.chatMessage;
+
+    public List<ChatRoom> findAllChatRoomsByMemberWithMessages(Member member) {
+
+        return jpaQueryFactory.selectFrom(chatRoom)
+                .leftJoin(chatRoom.sender, sender).fetchJoin()
+                .leftJoin(chatRoom.receiver, receiver).fetchJoin()
+                .leftJoin(chatRoom.chatMessages, chatMessage).fetchJoin()
+                .where(
+                        chatRoom.sender.eq(member)
+                        .or(chatRoom.receiver.eq(member))
+                )
+                .distinct()
+                .fetch();
+    }
+}
