bfedb5dc3a4967f2ceff121cf2df637554cce46c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-18 16:09:50 +0900

    refactor: User 객체 대신 인증된 사용자의 객체를 바로 가져오도록 리팩토링

--- src/main/java/kr/co/springtricount/controller/BalanceController.java
+++ src/main/java/kr/co/springtricount/controller/BalanceController.java
@@ -7,8 +7,6 @@ import kr.co.springtricount.service.service.BalanceService;
 import kr.co.springtricount.service.dto.response.BalanceResDTO;
 import kr.co.springtricount.service.service.SettlementService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,10 +24,9 @@ public class BalanceController {
     private final SettlementService settlementService;
 
     @GetMapping("/{settlement_id}")
-    public ResponseFormat<List<BalanceResDTO>> findBalanceBySettlement(@AuthenticationPrincipal User currentMember,
-                                                                       @PathVariable("settlement_id") Long settlementId) {
+    public ResponseFormat<List<BalanceResDTO>> findBalanceBySettlement(@PathVariable("settlement_id") Long settlementId) {
 
-        SettlementResDTO settlement = settlementService.findSettlementById(currentMember, settlementId);
+        SettlementResDTO settlement = settlementService.findSettlementById(settlementId);
 
         return ResponseFormat.successMessageWithData(
                 ResponseStatus.SUCCESS_EXECUTE,
