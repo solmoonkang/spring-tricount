0b89a5540d6453b79058702089d8c772b0561c7e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-20 23:07:06 +0900

    chore: 로그인 DTO 이름 수정

--- src/main/java/kr/co/springtricount/service/dto/LoginReqDTO.java
+++ src/main/java/kr/co/springtricount/service/dto/LoginReqDTO.java
@@ -2,7 +2,7 @@ package kr.co.springtricount.service.dto;
 
 import jakarta.validation.constraints.NotNull;
 
-public record LoginDTO(
+public record LoginReqDTO(
         @NotNull(message = "아이디를 입력해주세요.") String identity,
         @NotNull(message = "비밀번호를 입력해주세요.") String password
 ) { }
