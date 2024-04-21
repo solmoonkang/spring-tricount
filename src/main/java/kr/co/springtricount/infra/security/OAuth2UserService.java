7bf5434d598b1011bc1c47d252fa7ad4956de4c8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-21 19:26:29 +0900

    feat: 회원 정보를 찾아서 없다면 새로운 회원을 생성할 수 있도록 구현

--- src/main/java/kr/co/springtricount/infra/security/OAuth2UserService.java
+++ src/main/java/kr/co/springtricount/infra/security/OAuth2UserService.java
@@ -1,33 +1,52 @@
 package kr.co.springtricount.infra.security;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
+import kr.co.springtricount.persistence.entity.member.Member;
+import kr.co.springtricount.persistence.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class OAuth2UserService extends DefaultOAuth2UserService {
 
-    private static final String AUTHORITIES_ROLE = "ROLE_ADMIN";
+    private final MemberRepository memberRepository;
 
     @Override
+    @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
-        OAuth2User oAuth2User = super.loadUser(userRequest);
+        // 1. 유저 정보(attributes)를 가져온다.
+        Map<String, Object> oAuth2UserAttributes = super.loadUser(userRequest).getAttributes();
 
-        List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList(AUTHORITIES_ROLE);
+        // 2. resistrationId를 가져온다. (third-party id)
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
-        String userNameAttributeName = userRequest.getClientRegistration()
-                .getProviderDetails()
-                .getUserInfoEndpoint()
-                .getUserNameAttributeName();
+        // 3. userNameAttributeName을 가져온다.
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
+                .getUserInfoEndpoint().getUserNameAttributeName();
 
-        return new DefaultOAuth2User(authorities, oAuth2User.getAttributes(), userNameAttributeName);
+        // 4. 유저 정보 DTO를 생성한다.
+        OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfo.of(registrationId, oAuth2UserAttributes);
+
+        // 5. 회원가입 및 로그인을 진행한다.
+        Member member = getOrSave(oAuth2UserInfo);
+
+        // 6. OAuth2User를 반환한다.
+        return new PrincipalDetails(member, oAuth2UserAttributes, userNameAttributeName);
+    }
+
+    private Member getOrSave(OAuth2UserInfo oAuth2UserInfo) {
+
+        Member member = memberRepository.findMemberByIdentity(oAuth2UserInfo.identity())
+                .orElseGet(oAuth2UserInfo::toEntity);
+
+        return memberRepository.save(member);
     }
 }
